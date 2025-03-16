81ace533187d92b5f0882e10c5bb2ccb2997fc21
Santhosh <san272thosh@gmail.com>
2025-03-16 09:06:23 +0530

    Update download_model.py

--- download_model.py
+++ download_model.py
@@ -5,4 +5,6 @@ file_id = "1yGnMCPgNzEID-iFqFKw9mzZl_zF-W68f"
 url = f"https://drive.google.com/uc?id={1yGnMCPgNzEID-iFqFKw9mzZl_zF-W68f}"
 
 # Download model
-gdown.download(https://drive.google.com/file/d/1yGnMCPgNzEID-iFqFKw9mzZl_zF-W68f/view?usp=drive_link, "Final_predication_SoftVoting.pkl", quiet=False)
+gdown.download(url, "Final_predication_SoftVoting.pkl", quiet=False)
+print("✅ Model downloaded successfully!")
+               
