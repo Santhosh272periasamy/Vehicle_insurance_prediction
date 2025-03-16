cd7bbe24f62c62c39afb58427ce0814772aefe53
Santhosh <san272thosh@gmail.com>
2025-03-16 08:48:43 +0530

    Create download_model.py

---
+++ download_model.py
@@ -0,0 +1,8 @@
+import gdown
+
+# Google Drive file ID
+file_id = "1yGnMCPgNzEID-iFqFKw9mzZl_zF-W68f"
+url = f"https://drive.google.com/uc?id={1yGnMCPgNzEID-iFqFKw9mzZl_zF-W68f}"
+
+# Download model
+gdown.download(https://drive.google.com/file/d/1yGnMCPgNzEID-iFqFKw9mzZl_zF-W68f/view?usp=drive_link, "Final_predication_SoftVoting.pkl", quiet=False)
